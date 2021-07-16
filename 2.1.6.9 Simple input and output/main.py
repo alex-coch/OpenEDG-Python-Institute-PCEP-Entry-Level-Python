44ff7c370905270798eeeaf2ccb31e45daf27502
alex-coch <a@deonop.com>
2021-07-16 12:26:29 +0400

    2.1.6.9 Simple input and output

---
+++ 2.1.6.9 Simple input and output/main.py
@@ -0,0 +1,15 @@
+# input a float value for variable a here
+a  = float(input())
+# input a float value for variable b here
+b  = float(input())
+
+# output the result of addition here
+print(a+b)
+# output the result of subtraction here
+print(a-b)
+# output the result of multiplication here
+print(a*b)
+# output the result of division here
+print(a/b)
+
+print("\nThat's all, folks!")
